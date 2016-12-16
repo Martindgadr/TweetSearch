e7976259ade796fb7fae11fefc6862f8cad61432
Martindg <martintrestadr@gmail.com>
2016-12-16 19:51:13 -0300

    V 1.0.0 - OLX Test final

--- Documents/workspace/TweetSearch/app/src/main/java/com/example/olx/tweetsearch/Integration/ApiConstants.java
+++ Documents/workspace/TweetSearch/app/src/main/java/com/example/olx/tweetsearch/Integration/ApiConstants.java
@@ -6,8 +6,8 @@ package com.example.olx.tweetsearch.Integration;
 
 public class ApiConstants {
 
-    public final static String CONSUMER_KEY = "8KboM4uO48I0buViUugcn6xcz";
-    public	final static String CONSUMER_SECRET = "8qWzQSGK6XKf1jHwflGDwAnbF5W9SATepWTtlauc3AxOKoiO50";
+    public final static String CONSUMER_KEY = "put_your_consumer_key_here";
+    public	final static String CONSUMER_SECRET = "put_your_cons_secret_here";
 
     public final static String TWITTER_SEARCH_URL = "https://api.twitter.com";
     public static final String BEARER_TOKEN_CREDENTIALS = CONSUMER_KEY + ":" + CONSUMER_SECRET;
